ffb2d117a60d3980ec50050a16495a7bc326b039
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2021-12-17 14:59:52 +0100

    Cleaning up documentation

--- 1_2_hello_world/main.rs
+++ 1_2_hello_world/main.rs
@@ -1,6 +1,3 @@
 fn main() {
     println!("Hello, world!"); // ! makes it a macro not a function
 }
-
-// to compile this: rustc main.rs
-// to run: ./main
--- 1_3_hello_cargo/hello_cargo/src/main.rs
+++ 1_3_hello_cargo/hello_cargo/src/main.rs
@@ -1,21 +1,3 @@
 fn main() {
     println!("Hello, world!");
 }
-
-/*
-build:
-cd ./hello_cargo
-cargo build
-
-run:
-./target/debug/hello_cargo
-
-build and run:
-cargo run
-
-build without generating executable(way faster than generating executable and can be used continuously while coding):
-cargo check
-
-release build:
-cargo build --release
-*/
