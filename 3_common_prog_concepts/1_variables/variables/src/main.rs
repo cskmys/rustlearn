025cfaf31583ecf0a04e1729591d7625e90efc9b
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2021-12-17 17:56:05 +0100

    refactoring folder org, variables project setup

---
+++ 3_common_prog_concepts/1_variables/variables/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    /*
+    let x = 5;
+    println!("The value of x is {}", x);
+
+    x = 6; // throws a compilation error cause by default x is immutable i,e, cannot be assigned to more than once
+    println!("The value of x is {}", x);
+    */
+}
