7e7e7a8390432575c98feb7ace5d16ad896f793d
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2021-12-17 14:29:26 +0100

    1.3 Hello cargo

---
+++ 1_3_hello_cargo/hello_cargo/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/*
+build:
+cd ./hello_cargo
+cargo build
+
+run:
+./target/debug/hello_cargo
+
+build and run:
+cargo run
+
+build without generating executable(way faster than generating executable and can be used continuously while coding):
+cargo check
+
+release build:
+cargo build --release
+*/
