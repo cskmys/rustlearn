c9bf79fb3f4afc0e35a8b8546d22c06967ceb735
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-01-19 13:06:19 +0100

    minigrep: case-sensitive search using environment variable

--- 12_io_project/minigrep/src/lib.rs
+++ 12_io_project/minigrep/src/lib.rs
@@ -1,21 +1,30 @@
 #[cfg(test)]
 mod tests {
-    use crate::search;
+    use crate::{search, search_case_insensitive};
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
-        let contents = "Rust:\nsafe, fast, productive.\nPick three.";
+        let contents = "Rust:\nsafe, fast, productive.\nPick three.\nDuct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "Rust:\nsafe, fast, productive.\nPick three.\nTrust me.";
+
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
+    }
 }
 
-use std::{fs, error};
+use std::{fs, error, env};
 
 pub struct Config {
     pub query: String,
-    pub filename: String
+    pub filename: String,
+    pub case_sensitive: bool // configuration option added to switch between the type of search based on the environment variable
 }
 
 impl Config {
@@ -25,15 +34,22 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config {query, filename})
+        Ok(Config {query, filename, case_sensitive})
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn error::Error>>{
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let res = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in res {
         println!("{}", line);
     }
 
@@ -44,6 +60,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut res = Vec::new();
     for line in contents.lines() {
         if line.contains(query) {
+            res.push(line);
+        }
+    }
+    res
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase(); // shadowing "query" with its lowercase "String" version
+    // "to_lowercase" generates new "String" and doesn't work well with all the unicode
+    // since we are reading arguments using "std::env::args()" we can't read unicode anyways
+    let mut res = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             res.push(line);
         }
     }
