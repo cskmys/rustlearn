6f8071b5020c1981282b0af04b3e53acb36415a9
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-01-19 07:48:58 +0100

    minigrep: refactoring parsing args to config struct constructor

--- 12_io_project/minigrep/src/main.rs
+++ 12_io_project/minigrep/src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for \"{}\"", config.query);
     println!("In file \"{}\"", config.filename);
@@ -18,11 +18,11 @@ struct Config{
     filename: String
 }
 
-fn parse_config(args: &[String]) -> Config { // "args" is owned by "main", and is borrowed by "parse_config"
-    let query = args[1].clone(); // hence if a variable in "parse_config" tries to take ownership, it would violate the borrowing rules
-    let filename = args[2].clone(); // therefore, we are cloning to take full ownership
-    // though this will cost a little in time n space, it makes code very simple and straight forward
-    // in this case, we are trading a little bit of performance for simplicity
+impl Config{
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-    Config {query, filename}
+        Config {query, filename}
+    }
 }
