c1c7c277551e5a142a137e5b67abd6fd87b1a5fc
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-01-19 07:41:10 +0100

    minigrep: refactoring grouping args as struct

--- 12_io_project/minigrep/src/main.rs
+++ 12_io_project/minigrep/src/main.rs
@@ -4,18 +4,25 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for \"{}\"", query);
-    println!("In file \"{}\"", filename);
+    println!("Searching for \"{}\"", config.query);
+    println!("In file \"{}\"", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config{
+    query: String,
+    filename: String
+}
 
-    (query, filename)
+fn parse_config(args: &[String]) -> Config { // "args" is owned by "main", and is borrowed by "parse_config"
+    let query = args[1].clone(); // hence if a variable in "parse_config" tries to take ownership, it would violate the borrowing rules
+    let filename = args[2].clone(); // therefore, we are cloning to take full ownership
+    // though this will cost a little in time n space, it makes code very simple and straight forward
+    // in this case, we are trading a little bit of performance for simplicity
+
+    Config {query, filename}
 }
