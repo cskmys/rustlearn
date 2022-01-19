5a51681c67ad34db86e06c354d921886080e28cf
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-01-19 08:43:37 +0100

    minigrep: returning n handling errors from run function

--- 12_io_project/minigrep/src/main.rs
+++ 12_io_project/minigrep/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, fs, process, error};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,12 +11,18 @@ fn main() {
     println!("Searching for \"{}\"", config.query);
     println!("In file \"{}\"", config.filename);
 
-    run(config);
+    if let Err(e) = run(config){
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
 
-fn run(config: Config){
-    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+fn run(config: Config) -> Result<(), Box<dyn error::Error>>{ // previously this function returned nothing in other words, it returned "()", so return type is "Return<(), ...>"
+    // "Box<dyn Error>" means a value of type that implements the "Error" trait
+    // this allows us to return error values of different datatype under different error cases
+    let contents = fs::read_to_string(config.filename)?; // upon an error, "?" operator returns from the function with an error value
     println!("With text:\n{}", contents);
+    Ok(()) // returning "()" meaning nothing upon success requires wrapping with "Ok" as the return is "Result" type
 }
 
 struct Config{
