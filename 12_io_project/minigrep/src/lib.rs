f822ed21047027b473265ba35d94b27e2e474731
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-01-19 11:02:05 +0100

    minigrep: tdd step 1

--- 12_io_project/minigrep/src/lib.rs
+++ 12_io_project/minigrep/src/lib.rs
@@ -1,3 +1,17 @@
+#[cfg(test)]
+mod tests {
+    use crate::search;
+
+    #[test]
+    fn one_result() { // building a test case
+        let query = "duct";
+        let contents = "Rust:\nsafe, fast, productive.\nPick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents)); // "search" is a function that you'd like to build via TDD
+        // it takes a query and contents of a file as a "&str" as input and return the vector of lines where the string is present as output
+    }
+}
+
 use std::{fs, error};
 
 pub struct Config {
@@ -21,4 +35,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn error::Error>>{
     let contents = fs::read_to_string(config.filename)?;
     println!("With text:\n{}", contents);
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> { // lifetime applied to "contents" and return as "contents" is the parameter that is connected to the output
+    // hence, "contents" should be alive at least as long as the output
+    vec![]
 }
